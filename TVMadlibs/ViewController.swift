e34fc679ae120209db124bf2b5050c1577ac2e45
WhyDoYouStare <tjaidon111@gmail.com>
2019-03-26 13:18:33 -0400

    changed background color

--- TVMadlibs/ViewController.swift
+++ TVMadlibs/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+
     override func viewDidLoad() {
+       
+        
+       view.backgroundColor = UIColor.red
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+
+    
+    
+    
 
 }
 

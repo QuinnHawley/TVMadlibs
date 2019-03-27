df3560ad89d4f270049eae7a325958a475234c64
QuinnHawley <quinn@jghawley.com>
2019-03-27 13:49:10 -0400

    white

--- TVMadlibs/ViewController.swift
+++ TVMadlibs/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        view.backgroundColor = UIColor.black
+        view.backgroundColor = UIColor.white
      
     }
 
